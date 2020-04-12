70e76c6edce21d107590a400c298052e2d69d406
luizhp <luizhp@yahoo.com>
2020-04-12 13:49:38 -0300

    fix: adjust RequiredField Validation name

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,7 +1,7 @@
 import { makeSignUpValidation } from './signup-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
-import { RequireFieldValidation } from './../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation } from './../../presentation/helpers/validators/required-field-validation'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 import { EmailValidator } from '../../presentation/protocols/email-validator'
@@ -21,7 +21,7 @@ describe('SignUpValidation Factory', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-      validations.push(new RequireFieldValidation(field))
+      validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     validations.push(new EmailValidation('email', makeEmailValidator()))
--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -1,7 +1,7 @@
 import { Validation } from './validation'
 import { MissingParamError } from './../../errors'
 
-export class RequireFieldValidation implements Validation {
+export class RequiredFieldValidation implements Validation {
   private readonly fieldName: string
   constructor (fieldName: string) {
     this.fieldName = fieldName
