a0a116a2dc05d5cb2b51c8bb4a523f9269dd5e30
luizhp <luizhp@yahoo.com>
2020-04-17 12:56:51 -0300

    test: ensure SignUpController returns 500 if Authentication throws

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -53,8 +53,7 @@ describe('Login Controller', () => {
     expect(authSpy).toHaveBeenCalledWith({
       email: 'any_email@mail.com',
       password: 'any_password'
-    }
-    )
+    })
   })
 
   test('Should return 401 if invalid credentials are provided', async () => {
