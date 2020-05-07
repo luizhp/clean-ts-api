ae76187107aee29286f1a6315ef10f075a643ee6
luizhp <luizhp@yahoo.com>
2020-05-06 21:22:34 -0300

    test: ensure SaveSurveyResultController returns 403 if invalid survey is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survery-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survery-result-controller.spec.ts
@@ -1,4 +1,6 @@
 import { SurveyModel, LoadSurveyById, HttpRequest } from './save-survery-result-controller-protocols'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { SaveSurveyResultController } from './save-survery-result-controller'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -46,5 +48,12 @@ describe('SaveSurveyResult Controller', () => {
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
     await sut.handle(makeFakeRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  test('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
