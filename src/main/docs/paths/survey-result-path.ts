c5f35403e4c30c32ebc1a276b3130b96dad8956d
luizhp <luizhp@yahoo.com>
2020-05-14 14:29:26 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -10,11 +10,11 @@ export const surveyResultPath = {
     parameters: [{
       in: 'path',
       name: 'surveyId',
+      description: 'Survey Id',
       required: true,
       schema: {
         type: 'string'
-      },
-      description: 'Survey Id'
+      }
     }],
     requestBody: {
       content: {
@@ -25,6 +25,45 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Survey'],
+    summary: 'API para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'Survey Id',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
